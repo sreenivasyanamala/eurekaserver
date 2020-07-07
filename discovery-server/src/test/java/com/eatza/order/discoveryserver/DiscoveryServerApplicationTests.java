9d1750f19ff42ca7e47a39111e846db329d032a1
sreenivasyanamala <sreenivasjava2@gmai.com>
2020-07-07 18:05:38 +0530

    initial commit

---
+++ discovery-server/src/test/java/com/eatza/order/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatza.order.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
